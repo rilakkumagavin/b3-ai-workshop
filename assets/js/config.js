0e19843f95461b9ed011f70e5bce3784254a6554
rilakkumagavin <rilakkumagavin@users.noreply.github.com>
2026-09-15 06:19:38 +0800

    Allow Apps Script cold start response time

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 window.B3_DEFAULT_CONFIG = Object.freeze({
   APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxLu8lcKUHmnP34pDrbC-45wyE_wCdQelz-KZCn-UblOwxpfrOxOP0EA-Kwedmjxki2qw/exec',
   WORKSHOP_ID: 'b3-demo',
-  REQUEST_TIMEOUT_MS: 20000
+  REQUEST_TIMEOUT_MS: 90000
 });
 window.B3_CONNECTION_KEY = 'b3-connection-settings-v1';
 window.B3_CONFIG = (() => {
@@ -12,3 +12,4 @@ window.B3_CONFIG = (() => {
     APPS_SCRIPT_WEB_APP_URL: typeof saved.apiUrl === 'string' ? saved.apiUrl : window.B3_DEFAULT_CONFIG.APPS_SCRIPT_WEB_APP_URL,
     GOOGLE_FORM_URL: typeof saved.formUrl === 'string' ? saved.formUrl : ''});
 })();
+
