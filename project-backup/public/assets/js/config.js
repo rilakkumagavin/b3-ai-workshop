f06b93b9297c0f082a1bf8664e9ac27015d60998
B3 Workshop Maintainer <gavin@csps.tyc.edu.tw>
2026-09-15 11:21:31 +0800

    Connect workshop to new Apps Script deployment

--- project-backup/public/assets/js/config.js
+++ project-backup/public/assets/js/config.js
@@ -1,6 +1,6 @@
 // Fill in your deployed Apps Script /exec URL. Empty means localStorage mode.
 window.B3_DEFAULT_CONFIG = Object.freeze({
-  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxLu8lcKUHmnP34pDrbC-45wyE_wCdQelz-KZCn-UblOwxpfrOxOP0EA-Kwedmjxki2qw/exec',
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwwulGIniUTQ3JFBcmo2hvMCTjScDsdB1KdZyFM2brf435Qp0R0bRAVCwIxKt0SEop0/exec',
   WORKSHOP_ID: 'b3-demo',
   REQUEST_TIMEOUT_MS: 90000
 });
